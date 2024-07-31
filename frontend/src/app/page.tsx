c5a1d8baf3cfdf7bfae8ca996f33cf74eec41fc2
Saransh Bangar <saranshbangad@gmail.com>
2024-07-31 10:50:54 +0530

    Added word flipping

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,17 @@
+import { FlipWords } from "@/components/ui/flip-words";
 import Link from "next/link";
 
 export default function Home() {
+
+  const words = ["Mask", "Extract", "Encrypt"];
+
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">PDF Processing</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-[#122547]">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        <FlipWords words={words} />your
+        <br />
+        files easily
+      </h1>
       <div className="space-x-4">
         <Link href="/extract">
           <p className="px-4 py-2 bg-blue-500 text-white rounded">Extract</p>
